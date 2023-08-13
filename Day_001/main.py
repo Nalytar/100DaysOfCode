4be3b03c6846eadbb4a77f6b553ab34e4515af7d
Michael Kelle <kelle.michael@web.de>
2023-08-13 21:16:03 +0200

    Day 001 : print(), input(), len() function and basic variable assignment

---
+++ Day_001/main.py
@@ -0,0 +1,40 @@
+# udemy Course - 100 Days of Code - Python
+
+# Exercise 1 - Printing
+# print() function
+print("Exercise 1")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print'")
+
+print("----------------------------------------------")
+# Exercise 2 - Fixing Errors
+# Fixing syntax mistakes, indentation errors and string concatenation
+
+# Like:
+# print('Exampele word")
+#     print("Next Line")
+# print(("Example concatentaion looks like this: word1 "+" word2)
+
+
+# Exercise 3 - Input Function
+# input() function in len() function in print() function, printing userinput
+print("Exercise 3")
+
+print(len(str(input("What is your name?"))))
+print("----------------------------------------------")
+
+
+# Exercise 4 - Variables
+# Switching the vales of variables
+print("Exercise 4")
+
+a = input("a: ")
+b = input("b: ")
+
+a, b = b, a
+
+print("a: " + a)
+print("b: " + b)
+print("----------------------------------------------")
