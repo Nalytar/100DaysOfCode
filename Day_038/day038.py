1bc5d04f0e077839a80f35482d2ed94f8d84f264
Michael <kelle.michael@web.de>
2024-05-13 22:53:39 +0200

    Day 038: API Requests for nutritionix and Sheety to get Burned Calories from workout input and write response into google sheet, Added Sheety API POST to Google Sheet to insert values

--- Day_038/day038.py
+++ Day_038/day038.py
@@ -1,5 +1,7 @@
 # udemy Course - 100 Days of Code - Python
 import json
+import datetime
+
 import requests as r
 
 with open('data.json', 'r') as json_file:
@@ -11,11 +13,34 @@ with open('data.json', 'r') as json_file:
 	print(headers)
 
 query = input("What did you do today as workout?")
-r = r.post(url, headers=headers, json={"query": query})
+response = r.post(url, headers=headers, json={"query": query})
 
-if r.status_code == 200:
-	print(r.json())
+if response.status_code == 200:
+	response = response.json()
+	print(response)
 else:
-	print(str(r.status_code) + " " + r.text)
+	print(str(response.status_code) + " " + response.text)
 
-#ToDo GoogleSheet API
+values = {"tabellenblatt1": {
+	"date": datetime.datetime.today().strftime("%d/%m/%Y"),
+	"time": datetime.datetime.today().strftime("%H:%M:%S"),
+	"exercise": response["exercises"][0]["name"],
+	"calories": response["exercises"][0]["nf_calories"],
+	"duration": response["exercises"][0]["duration_min"]
+	}
+}
+
+print("--------------------------------------------------------------------------------------------")
+
+with open('data.json', 'r') as json_file:
+	data = json.load(json_file)
+	url = data["SHEETY"]["URL"]
+	headers = {}
+	for key, value in data['SHEETY']['HEADER_PARAM'].items():
+		headers[key] = value
+
+response = r.post(url, headers=headers, json=values)
+
+if response.status_code == 200:
+	r = response.json()
+	print(r)
